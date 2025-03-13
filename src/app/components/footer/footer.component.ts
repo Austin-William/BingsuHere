75d0269227239262ada2941cdb956490c6c1281e
Austin-William Lo <55129304+Austin-William@users.noreply.github.com>
2025-03-13 22:38:52 +0100

    [ADD] footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -2,18 +2,19 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 
-import { LinkComponent } from '../../components/link/link.component';
-
-import { links } from "../../data/links.json"; 
+import { links, socials } from "../../data/links.json";
+import { shops } from "../../data/shops.json";
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink, MatIconModule, LinkComponent],
+  imports: [RouterLink, MatIconModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
   host: { 'id': crypto.getRandomValues(new Uint32Array(1))[0].toString() }
 })
 
 export class FooterComponent {
-  categories_links = links;
+  categories_links: any = links;
+  socials: any = socials;
+  shops: any = shops;
 }
