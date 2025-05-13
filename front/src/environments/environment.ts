ea85ef34566e1ed6aa6cbe0cd4457b850d5f9e0b
Austin-William Lo <55129304+Austin-William@users.noreply.github.com>
2025-05-13 20:33:23 +0200

    [UPDATE] target url server

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: "https://prominent-ferdinanda-austin-william-154ee239.koyeb.app",
-  apiPort: "8080"
+  apiUrl: "https://prominent-ferdinanda-austin-william-154ee239.koyeb.app/server",
+  apiPort: ""
 };
